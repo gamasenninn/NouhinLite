4792356a7d013b488740f9cd2a520f1772d98bf6
gamasenninn <61409504+gamasenninn@users.noreply.github.com>
2020-12-27 14:31:14 +0900

    add new file

---
+++ btNou.py
@@ -0,0 +1,48 @@
+from bottle import route, run, jinja2_template as template, HTTPResponse
+import sqlite3
+import json
+
+@route('/')
+def index():
+    return template('menu.html',title='test',name="AAAA")
+
+@route('/nouhin')
+def nouhin():
+    name = "Hoge"
+    #return name
+
+    conn = sqlite3.connect('nousei.db')
+    conn.row_factory = sqlite3.Row
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM 納品' )    
+
+    nous = c.fetchall()
+
+    c2 = conn.cursor()
+    c2.execute('SELECT * FROM 納品明細' )    
+
+    ndtails = c2.fetchall()
+    
+    return template('hello.html', title='test', name=name,nous =nous,ndtails=ndtails ) #変更
+
+@route('/api')
+def api():
+    header = {"Content-Type": "application/json"}
+    body = {"message": "OK"}
+    conn = sqlite3.connect('nousei.db')
+    conn.row_factory = sqlite3.Row
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM 納品' )
+    nous = json.dumps(c.fetchall())
+
+    
+    res = HTTPResponse(status=200, body=nous, headers=header)
+    return res
+
+
+
+
+
+run(host='localhost', port=8080, reload=True, debug=True)
