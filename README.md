6672193e6dcd1bb240d3445f93351309ca192d7e
gamasenninn <gamasenninn@yahoo.co.jp>
2021-01-02 23:05:40 +0900

    Modify ReadMe

--- README.md
+++ README.md
@@ -120,4 +120,99 @@ nous = c.fetchall()
 print (nous)
 ```
 
+## DBへのアクセスの方針
+DBへのアクセスはなるべく生のSQLを書かないようにしたいと思っています。  
+SQLインジェクション防止とプログラムのメンテナンス性向上のためです。SQLite以外のDBMSでも簡単に移行が可能となります。    
+とはいえORMなどを導入するまではいかないのでDBアクセスのためのラッパーを用意したいと思います。  
+一方フロントからのデータ入出力はすべてajax通信によるものなのでJsonでのやり取りになります。  
+次のような関数群を想定しています。  
+```
+import sqlite3
+import json
+
+def sqlify_for_insert(string_iterable):
+    return f'({",".join(string_iterable)})'
+
+def sqlify_for_update(string_iterable):
+    return f'{"=?,".join(string_iterable)+"=?"}'
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+#----- Insert operation
+
+def dict_insert(conn, table_name, d):
+    conn.execute(
+        'insert into {table_name} {keys} values {replacement_fields}'.format(
+            table_name=table_name,
+            keys=sqlify_for_insert(d), 
+            replacement_fields=sqlify_for_insert('?'*len(d)) 
+        ),
+        tuple(d.values())
+    )
+
+def json_insert(conn,table_name,j):
+    dic_d = json.loads(j)
+    return dict_insert(conn,table_name,dic_d)
+
+#------ Update operation
+
+def dict_update(conn,table_name,d,key_name):
+    v = d.pop(key_name)
+    vl=[]
+    vl.append(v)
+    
+    conn.execute(
+        'update {table_name} set {key_values} where {key_name} = ? '.format(
+            table_name=table_name,
+            key_values=sqlify_for_update(d),
+            key_name = key_name
+        ),
+        tuple(d.values())+tuple(vl)
+    )
+
+def json_update(conn,table_name,j,key_name):
+    dic_j = json.loads(j)
+    return dict_update(conn,table_name,dic_j,key_name)
+
+#----- delete operation
+
+def dict_delete(conn, table_name, d, key_name):
+    v = d.pop(key_name)
+    vl=[]
+    vl.append(v)
+    conn.execute(
+        'delete from {table_name} where {keys} = ?'.format(
+            table_name=table_name,
+            keys=key_name
+        ),
+        tuple(vl)
+    )
+
+def json_delete(conn,table_name,j,key_name):
+    dic_j = json.loads(j)
+    return dict_delete(conn,table_name,dic_j,key_name)
+
+
+#------ select operation
+
+def json_select_all(conn, table_name):
+    conn.row_factory = dict_factory
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM '+table_name )
+
+    nous = c.fetchall()
+
+    return nous
+
+```
+
+
+
+
+
 
--- sqltest2.py
+++ sqltest2.py
@@ -1,43 +1,87 @@
 import sqlite3
 import json
 
-
-def sqlify_list(string_iterable):
+def sqlify_for_insert(string_iterable):
     return f'({",".join(string_iterable)})'
 
-def sqlify_list2(string_iterable):
-    return f'({"=?,".join(string_iterable)})'
+def sqlify_for_update(string_iterable):
+    return f'{"=?,".join(string_iterable)+"=?"}'
 
-#    return ','.join([f'{key} = {value}' for key, value in string_iterable.items()])
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 #----- Insert operation
 
-def json_insert(conn, table_name, d):
+def dict_insert(conn, table_name, d):
     conn.execute(
         'insert into {table_name} {keys} values {replacement_fields}'.format(
             table_name=table_name,
-            keys=sqlify_list(d), 
-            replacement_fields=sqlify_list('?'*len(d)) 
+            keys=sqlify_for_insert(d), 
+            replacement_fields=sqlify_for_insert('?'*len(d)) 
         ),
         tuple(d.values())
     )
 
+def json_insert(conn,table_name,j):
+    dic_d = json.loads(j)
+    return dict_insert(conn,table_name,dic_d)
+
 #------ Update operation
 
-def json_update(conn,table_name,d,key_name):
-    print("key_nmae="+key_name)
-    v = [v for k,v in data.items() if k == key_name]
-    #key_value = v[0] if len(v) else ''
-    #print("key_value="+v)
-    print(
+def dict_update(conn,table_name,d,key_name):
+    v = d.pop(key_name)
+    vl=[]
+    vl.append(v)
+    
+    conn.execute(
         'update {table_name} set {key_values} where {key_name} = ? '.format(
             table_name=table_name,
-            key_values=sqlify_list2(d),
+            key_values=sqlify_for_update(d),
             key_name = key_name
         ),
-        tuple(d.values())+tuple(v)
+        tuple(d.values())+tuple(vl)
     )
 
+def json_update(conn,table_name,j,key_name):
+    dic_j = json.loads(j)
+    return dict_update(conn,table_name,dic_j,key_name)
+
+#----- delete operation
+
+def dict_delete(conn, table_name, d, key_name):
+    v = d.pop(key_name)
+    vl=[]
+    vl.append(v)
+    conn.execute(
+        'delete from {table_name} where {keys} = ?'.format(
+            table_name=table_name,
+            keys=key_name
+        ),
+        tuple(vl)
+    )
+
+def json_delete(conn,table_name,j,key_name):
+    dic_j = json.loads(j)
+    return dict_delete(conn,table_name,dic_j,key_name)
+
+
+#------ select operation
+
+def json_select_all(conn, table_name):
+    conn.row_factory = dict_factory
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM '+table_name )
+
+    nous = c.fetchall()
+
+    return nous
+
+
+#----------- main block -----------
 
 conn = sqlite3.connect(':memory:')
 
@@ -54,28 +98,58 @@ CREATE TABLE "納品" (
 '''
 conn.execute(sql)
 
-print("Created Table")
+print("\n-----Created Table------")
 
 #-----Insert data------
+print("\n-----insert into Table------")
 
-#data = {'ID': 1,'納品日': "2020/11/01",'納品先': "ABC商店",'担当者': "小野",'摘要': "特別注文"}
 data = {'納品日': "2020/11/01",'納品先': "ABC商店",'担当者': "小野",'摘要': "特別注文"}
-#d = {'a': 1, 'b': 2, 'c': 3}
-json_insert(conn, '納品', data)
+dict_insert(conn, '納品', data)
 
-#data = {'ID': 3,'納品日': "2020/11/02",'納品先': "DEF商店",'担当者': "小野",'摘要': "特別注文"}
 data = {'納品日': "2020/11/02",'納品先': "DEF商店",'担当者': "小野",'摘要': "特別注文"}
-json_insert(conn, '納品', data)
+j_data = json.dumps(data)
+json_insert(conn, '納品', j_data)
 
 
-#----Select adta ------
+#----Select data ------
+print("\n-----select from Table------")
 c = conn.cursor()
 c.execute('SELECT * FROM 納品' )
-
 nous = c.fetchall()
-
 print (nous)
 
 #-----Update data------
-data = {'ID': 3,'納品日': "2020/11/02",'納品先': "DEF商店",'担当者': "小野",'摘要': "特別注文"}
-json_update(conn,'納品',data,'担当者')
+print("\n-----Update Table------")
+data = {'ID': 2,'納品日': "2020/11/02",'納品先': "更新テスト",'担当者': "小野",'摘要': "特別注文"}
+dict_update(conn,'納品',data,'ID')
+
+data = {'納品日': "2020/11/02",'担当者': "小野",'摘要': "更新テスト"}
+j_data = json.dumps(data)
+json_update(conn,'納品',j_data,'担当者')
+
+#----Select data ------
+print("\n-----select from Table------")
+c.execute('SELECT * FROM 納品' )
+nous = c.fetchall()
+print (nous)
+
+#----Select data JSON Format------
+print("\n-----select data JSON ------")
+nous = json_select_all(conn,'納品')
+print(nous)
+
+#-----Delete data------
+print("\n-----Delete Table------")
+data = {'ID': 2}
+dict_delete(conn,'納品',data,'ID')
+
+data = {'ID': 1}
+j_data = json.dumps(data)
+json_delete(conn,'納品',j_data,'ID')
+
+#----Select data ------
+print("\n-----select from Table------")
+c.execute('SELECT * FROM 納品' )
+nous = c.fetchall()
+print (nous)
+
