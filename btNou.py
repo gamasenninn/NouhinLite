6bc917aa638a671c93ab34a665183f2d35e905b6
gamasenninn <gamasenninn@yahoo.co.jp>
2021-01-03 04:05:45 +0900

    Add wrapper and modify btNou etc

--- btNou.py
+++ btNou.py
@@ -1,54 +1,41 @@
-from bottle import route, run, jinja2_template as template, HTTPResponse
+from bottle import route, run, jinja2_template as template,HTTPResponse,response,request
 import sqlite3
 import json
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+from sqlwrap import *
 
 
 @route('/')
 def index():
     return template('menu.html',title='test',name="AAAA")
 
-
 @route('/nouhin')
 def nouhin():
     name = "Hoge"
-    #return name
 
     conn = sqlite3.connect('nousei.db')
-    conn.row_factory = sqlite3.Row
 
-    c = conn.cursor()
-    c.execute('SELECT * FROM 納品' )    
+    j_nous = json_select_all(conn,'納品')
+    nous = json.loads(j_nous)
 
-    nous = c.fetchall()
-
-    c2 = conn.cursor()
-    c2.execute('SELECT * FROM 納品明細' )    
-
-    ndtails = c2.fetchall()
+    j_ndtails = json_select_all(conn,'納品明細')
+    ndtails = json.loads(j_ndtails)
     
     return template('hello.html', title='test', name=name,nous =nous,ndtails=ndtails ) #変更
 
 
-@route('/api/<table>')
-def api(table='納品'):
+@route('/api/<table>',method=['GET','POST'])
+def api(table):
     header = {"Content-Type": "application/json"}
-
     conn = sqlite3.connect('nousei.db')
-    conn.row_factory = dict_factory
 
-    c = conn.cursor()
-    c.execute('SELECT * FROM '+table )
-    nous = json.dumps(c.fetchall())
-   
-    res = HTTPResponse(status=200, body=nous, headers=header)
-    return res
+    if request.method == 'GET':
+        res = json_select_all(conn,table)
+    else:
+        res = dict_insert(conn,table,request.json)
+        conn.commit()
+    conn.close()
+    return HTTPResponse(status=200, body=res, headers=header)
 
-##
+
 
 run(host='localhost', port=8080, reload=True, debug=True)
